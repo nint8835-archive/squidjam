2d9adb012b2d137f23cd7a8fdd3cb669459255d5
Riley Flynn <riley@rileyflynn.me>
2024-01-28 10:10:55 -0330

    Buttons to perform actions

--- src/Squidjam.App/Frontend/src/components/Player.tsx
+++ src/Squidjam.App/Frontend/src/components/Player.tsx
@@ -1,3 +1,4 @@
+import { usePerformAction } from '../queries/api/squidjamComponents';
 import * as Schema from '../queries/api/squidjamSchemas';
 import { useStore } from '../store';
 import { cn } from '../util';
@@ -5,14 +6,44 @@ import { cn } from '../util';
 const playerColours = ['bg-red-900', 'bg-purple-900', 'bg-blue-900'];
 
 export default function Player({ player, playerIndex }: { player: Schema.Player; playerIndex: number }) {
-    const { player: currentPlayer } = useStore();
+    const {
+        currentGame: { id: gameId },
+        player: currentPlayer,
+    } = useStore();
+    const { mutateAsync: performAction } = usePerformAction({});
+
     const isCurrentPlayer = player.id === currentPlayer;
 
     const playerColour = playerColours[playerIndex % 3];
 
+    const actions: { label: string; body: Schema.SelectClass | Schema.Ready | Schema.EndTurn }[] = [
+        { label: 'Grack', body: { type: 'SelectClass', class: { type: 'Grack' }, player: currentPlayer } },
+        { label: 'Gump', body: { type: 'SelectClass', class: { type: 'Gump' }, player: currentPlayer } },
+        { label: 'Ready', body: { type: 'Ready', player: currentPlayer } },
+        { label: 'End Turn', body: { type: 'EndTurn', player: currentPlayer } },
+    ];
+
     return (
         <div className={cn('p-2', playerColour)}>
             <pre>{JSON.stringify(player, undefined, 2)}</pre>
+            {isCurrentPlayer && (
+                <div className="flex justify-between gap-4 px-16 pt-2">
+                    {actions.map(({ label, body }) => (
+                        <button
+                            key={label}
+                            className="flex-1 rounded-md bg-zinc-300 p-2 text-zinc-900 transition-all hover:bg-zinc-400"
+                            onClick={async () => {
+                                await performAction({
+                                    pathParams: { gameId },
+                                    body,
+                                });
+                            }}
+                        >
+                            {label}
+                        </button>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
--- src/Squidjam.App/Frontend/src/routes/Game.tsx
+++ src/Squidjam.App/Frontend/src/routes/Game.tsx
@@ -6,6 +6,7 @@ export default function GamePage() {
 
     return (
         <div className="flex h-full flex-col">
+            <div>{JSON.stringify(currentGame.state)}</div>
             <div className="flex-1">
                 {currentGame?.players.map((player, playerIndex) =>
                     player.id === currentPlayer ? null : (
